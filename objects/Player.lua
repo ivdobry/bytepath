1d3ae5809a3ed8c070533cf0bab3d6c498c12e92
unknown <ivan@rzilient.club>
2024-11-28 16:23:17 +0100

    add fighter ship

--- objects/Player.lua
+++ objects/Player.lua
@@ -17,15 +17,50 @@ function Player:new(area, x, y, opts)
 
     self.base_max_v = 100
     self.max_v = self.base_max_v
-
     self.trail_color = skill_point_color
 
-    self.timer:every(0.01, function()
-        self.area:addGameObject('TrailParticle',
-            self.x - self.w * math.cos(self.r), self.y - self.h * math.sin(self.r),
-            { parent = self, r = random(2, 4), d = random(0.15, 0.25), color = getColor(self.trail_color) })
-    end)
+    self.ship = 'Fighter'
+    self.polygons = {}
 
+    if self.ship == 'Fighter' then
+        self.polygons[1] = {
+            self.w, 0,
+            self.w / 2, -self.w / 2,
+            -self.w / 2, -self.w / 2,
+            -self.w, 0,
+            -self.w / 2, self.w / 2,
+            self.w / 2, self.w / 2,
+        }
+
+        self.polygons[2] = {
+            self.w / 2, -self.w / 2,
+            0, -self.w,
+            -self.w - self.w / 2, -self.w,
+            -3 * self.w / 4, -self.w / 4,
+            -self.w / 2, -self.w / 2,
+        }
+
+        self.polygons[3] = {
+            self.w / 2, self.w / 2,
+            -self.w / 2, self.w / 2,
+            -3 * self.w / 4, self.w / 4,
+            -self.w - self.w / 2, self.w,
+            0, self.w,
+        }
+    end
+
+    self.timer:every(0.01, function()
+        if self.ship == 'Fighter' then
+            self.area:addGameObject('TrailParticle',
+                self.x - 0.9 * self.w * math.cos(self.r) + 0.2 * self.w * math.cos(self.r - math.pi / 2),
+                self.y - 0.9 * self.w * math.sin(self.r) + 0.2 * self.w * math.sin(self.r - math.pi / 2),
+                { parent = self, r = random(2, 4), d = random(0.15, 0.25), color = getColor(self.trail_color) })
+            self.area:addGameObject('TrailParticle',
+                self.x - 0.9 * self.w * math.cos(self.r) + 0.2 * self.w * math.cos(self.r + math.pi / 2),
+                self.y - 0.9 * self.w * math.sin(self.r) + 0.2 * self.w * math.sin(self.r + math.pi / 2),
+                { parent = self, r = random(2, 4), d = random(0.15, 0.25), color = getColor(self.trail_color) })
+        end
+    end)
 
     self.timer:every(0.24, function() self:shoot() end)
     self.timer:every(5, function() self:tick() end)
@@ -36,6 +71,23 @@ end
 function Player:update(dt)
     Player.super.update(self, dt)
 
+
+    -- Boost
+    self.max_v = self.base_max_v
+    self.boosting = false
+    if input:down('up') then
+        self.boosting = true
+        self.max_v = 1.5 * self.base_max_v
+    end
+
+    if input:down('down') then
+        self.boosting = true
+        self.max_v = 0.5 * self.base_max_v
+    end
+
+    if self.boosting then self.trail_color = boost_color end
+
+    -- Movement
     if input:down('left') then
         self.r = self.r - self.rv * dt
     end
@@ -46,18 +98,6 @@ function Player:update(dt)
     self.v = math.min(self.v + self.a * dt, self.max_v)
     self.collider:setLinearVelocity(self.v * math.cos(self.r), self.v * math.sin(self.r))
 
-    self.max_v = self.base_max_v
-    self.boosting = false
-    if input:down('up') then
-        self.boosting = true
-        self.max_v = 1.5 * self.base_max_v
-    end
-    if input:down('down') then
-        self.boosting = true
-        self.max_v = 0.5 * self.base_max_v
-    end
-    self.trail_color = skill_point_color
-    if self.boosting then self.trail_color = boost_color end
 
     if self.x < 0 then self:die() end
     if self.y < 0 then self:die() end
@@ -66,8 +106,22 @@ function Player:update(dt)
 end
 
 function Player:draw()
-    love.graphics.circle('line', self.x, self.y, self.w)
-    love.graphics.line(self.x, self.y, self.x + 2 * self.w * math.cos(self.r), self.y + 2 * self.w * math.sin(self.r))
+    pushRotate(self.x, self.y, self.r)
+    love.graphics.setColor(default_color)
+    for _, vertice_group in ipairs(self.polygons) do
+        local points = {}
+
+        for i, point in ipairs(vertice_group) do
+            if i % 2 == 1 then
+                table.insert(points, self.x + point + random(-1, 1))
+            else
+                table.insert(points, self.y + point + random(-1, 1))
+            end
+        end
+
+        love.graphics.polygon('line', points)
+    end
+    love.graphics.pop()
 end
 
 function Player:shoot()
